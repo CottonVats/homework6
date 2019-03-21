6d4346adf0c57112476c78a188449b8a26b45e1c
CottonVats <47786883+CottonVats@users.noreply.github.com>
2019-03-21 19:55:35 +0300

    Add files via upload

--- VK.py
+++ VK.py
@@ -11,8 +11,8 @@ auth_params = {
 }
 
 print("?".join((oauth_url, urlencode(auth_params))))
-token1 = '86294bd197a69804253b5e122219151f85ef4d3aee43ab1a4fe5279c9b028799e279e243e66265fc84336'
-token2 = 'cfc6765663cb1406789fc7da7689c25f7723f4539eb8b02dcc2769141de4ee3f44e7def6cbf87ddd79701'
+token1 = '5e354313b78f73ef38950d096b4b462bdcd2847acfac2c4c8bd244c0d6609e48469871f1409b32bd14d6f'
+token2 = '3082f37da725283f5e4c593ad93f3aab248d86fa7a79b93a597f6994923ba00046aee6e7d5c698646b0d3'
 class VK_user:
     def __init__(self, token):
         self.token = token
@@ -23,6 +23,8 @@ class VK_user:
         self.resp = self.response.json()['response']
         self.id = self.resp[0]['id']
         self.user = 'www.vk.com/'+str(self.id)
+    def __str__(self):
+        return self.user
     def get_multual_friends(self,  class_obj):
         other_token = class_obj.token
         self.params = {
@@ -39,7 +41,10 @@ class VK_user:
         set_response2 = set(response2.json()['response']['items'])
         print(set_response1.intersection(set_response2))
         return set_response1.intersection(set_response2)
+    def __and__(self, class_obj):
+        self.get_multual_friends(class_obj)
 
 user_me = VK_user(token1)
 user_other = VK_user(token2)
-user_me.get_multual_friends(user_other)
+user_me&user_other
+print(user_other)
